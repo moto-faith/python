e26ae65368048dc0acd2083a3c71b17437eb89d6
1064030249 <1064030249@qq.com>
2018-08-28 06:54:32 +0800

    网络编程1服务端

--- test-10.py
+++ test-10.py
@@ -1,16 +1,28 @@
-import numpy as np
-import pandas as pd
-f=open('kongnan.csv',encoding='gb18030',errors='ignore')
-data = pd.read_csv(f)
-# print(data.Location.value_counts()[:10])
-data['Date']= data.Date.map(lambda x:x.split('/')[-1])
-# print(data.Date.value_counts()[:10])
-# print(data.groupby('Date')[['Fatalities']].sum().sort_values(by='Fatalities',ascending=False))
-data['taoshenglv']=(data.Aboard-data.Fatalities)/data.Aboard
-# print(data['taoshenglv'])
-data1=data[['Date','Location','Type','Aboard','Fatalities','taoshenglv']]
-# print(data1.describe())
-# print(data1[data.Aboard>600])
-# print(data.groupby('Location')[['Fatalities']].sum())
-# print(data1.groupby('Location')[['taoshenglv']].mean().sort_values(by='taoshenglv',ascending=False))
-# print(data.groupby('Type')[['taoshenglv']].mean().sort_values(by='taoshenglv',ascending=False))
+# 导入 socket、sys 模块
+import socket
+import sys
+
+# 创建 socket 对象
+serversocket = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM)
+
+# 获取本地主机名
+host = socket.gethostname()
+
+port = 9999
+
+# 绑定端口号
+serversocket.bind((host, port))
+
+# 设置最大连接数，超过后排队
+serversocket.listen(5)
+
+while True:
+    # 建立客户端连接
+    clientsocket, addr = serversocket.accept()
+
+    print("连接地址: %s" % str(addr))
+
+    msg = '欢迎访问菜鸟教程！' + "\r\n"
+    clientsocket.send(msg.encode('utf-8'))
+    clientsocket.close()
